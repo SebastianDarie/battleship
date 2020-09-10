bed80743ed939fd46c6b7e3de9efa2e418ce202e
SebastianDarie <s.d.01.2002@gmail.com>
2020-09-10 19:02:30 +0300

    rotate, render, big mess

---
+++ src/game/render.js
@@ -0,0 +1,127 @@
+const render = () => {
+	//const ships = document.querySelectorAll('#ships')
+	const plrBoard = document.querySelector('#plr1')
+	const aiBoard = document.querySelector('#plr2')
+	const destroyer = document.querySelector('.destroyer-ship')
+	const cruiser = document.querySelector('.cruiser-ship')
+	const cruiser2 = document.querySelector('.cruiser-ship-2')
+	const battleship = document.querySelector('.battleship-ship')
+	const carrier = document.querySelector('.carrier-ship')
+	const rotateBtn = document.querySelector('.rotate')
+
+	const plrSquares = []
+	const aiSquares = []
+	let isHorizontal = true
+	let direction
+
+	const shipArr = [
+		{
+			name: 'destroyer',
+			directions: [
+				[0, 1],
+				[0, 10],
+			],
+		},
+		{
+			name: 'cruiser',
+			directions: [
+				[0, 1, 2],
+				[0, 10, 20],
+			],
+		},
+		{
+			name: 'cruiser',
+			directions: [
+				[0, 1, 2],
+				[0, 10, 20],
+			],
+		},
+		{
+			name: 'battleship',
+			directions: [
+				[0, 1, 2, 3],
+				[0, 10, 20, 30],
+			],
+		},
+		{
+			name: 'carrier',
+			directions: [
+				[0, 1, 2, 3, 4],
+				[0, 10, 20, 30, 40],
+			],
+		},
+	]
+
+	function createBoard(board, squares) {
+		for (let i = 0; i < 100; i++) {
+			const square = document.createElement('div')
+
+			square.classList.add('board-cell')
+			square.dataset.id = i
+
+			board.appendChild(square)
+			squares.push(square)
+		}
+	}
+
+	function generateShip(ship) {
+		let randomDirection = Math.floor(Math.random() * ship.directions.length)
+		let curr = ship.directions[randomDirection]
+
+		if (randomDirection === 0) direction = 1
+		if (randomDirection === 1) direction = 10
+
+		let randomPoint = Math.abs(
+			Math.floor(
+				Math.random() * aiSquares.length -
+					ship.directions[0].length * direction
+			)
+		)
+
+		const isTaken = curr.some((index) =>
+			aiSquares[randomPoint + index].classList.contains('taken')
+		)
+		const isAtRightEdge = curr.some(
+			(index) => (randomPoint + index) % 10 === 10 - 1
+		)
+		const isAtLeftEdge = curr.some(
+			(index) => (randomPoint + index) % 10 === 0
+		)
+
+		if (!isTaken && !isAtRightEdge && !isAtLeftEdge)
+			curr.forEach((index) => {
+				aiSquares[randomPoint + index].classList.remove('board-cell')
+				aiSquares[randomPoint + index].classList.add('taken', ship.name)
+			})
+		else generateShip(ship)
+	}
+
+	function rotateShips() {
+		isHorizontal ? verticalClasses() : verticalClasses()
+	}
+
+	function verticalClasses() {
+		destroyer.classList.toggle('destroyer-ship-vertical')
+		cruiser.classList.toggle('cruiser-ship-vertical')
+		cruiser2.classList.toggle('cruiser-ship-vertical')
+		battleship.classList.toggle('battleship-ship-vertical')
+		carrier.classList.toggle('carrier-ship-vertical')
+
+		isHorizontal = !isHorizontal
+	}
+
+	createBoard(plrBoard, plrSquares)
+	createBoard(aiBoard, aiSquares)
+
+	generateShip(shipArr[0])
+	generateShip(shipArr[1])
+	generateShip(shipArr[2])
+	generateShip(shipArr[3])
+	generateShip(shipArr[4])
+
+	rotateBtn.addEventListener('click', rotateShips)
+
+	return { createBoard }
+}
+
+export default render
--- src/index.js
+++ src/index.js
@@ -1,46 +1,39 @@
+import render from './game/render'
+
 import './style.css'
 
 //Temporary Code
 
-const ships = document.querySelectorAll('#ships')
-const plrBoard = document.querySelector('#plr1')
-const aiBoard = document.querySelector('#plr2')
+render()
 
-for (let i = 0; i < 100; i++) {
-	const div1 = document.createElement('div')
-	const div2 = document.createElement('div')
-	div1.classList.add('board-cell')
-	div2.classList.add('board-cell')
+// const ships = document.querySelectorAll('#ships')
+// const plrBoard = document.querySelector('#plr1')
+// const aiBoard = document.querySelector('#plr2')
 
-	plrBoard.appendChild(div1)
-	aiBoard.appendChild(div2)
-}
+// ships.forEach((ship) => {
+// 	ship.addEventListener('dragstart', () => {
+// 		ship.classList.add('dragging')
+// 	})
 
-ships.forEach((ship) => {
-	ship.addEventListener('dragstart', () => {
-		ship.classList.add('dragging')
-	})
+// 	ship.addEventListener('dragend', () => {
+// 		ship.classList.remove('dragging')
+// 	})
+// })
 
-	ship.addEventListener('dragend', () => {
-		ship.classList.remove('dragging')
-	})
-})
+// plrBoard.addEventListener('dragover', (e) => {
+// 	e.preventDefault()
 
-plrBoard.addEventListener('dragover', (e) => {
-	e.preventDefault()
+// 	const afterEl = getDragEl(plrBoard, e.clientY)
+// 	const dragged = document.querySelector('.dragging')
+// 	dragged.style.zIndex = 1
+// 	console.log(afterEl)
 
-	const afterEl = getDragEl(plrBoard, e.clientY)
-	const dragged = document.querySelector('.dragging')
-	console.log(afterEl)
-
-	if (afterEl === null) {
-		plrBoard.appendChild(dragged)
-	} else {
-		plrBoard.insertBefore(dragged, afterEl)
-
-		// afterEl.replaceWith(dragged)
-	}
-})
+// 	if (afterEl === null) {
+// 		plrBoard.appendChild(dragged)
+// 	} else {
+// 		plrBoard.insertBefore(dragged, afterEl)
+// 	}
+// })
 
 function getDragEl(board, y) {
 	const squares = [...board.querySelectorAll('.board-cell')]
@@ -50,7 +43,7 @@ function getDragEl(board, y) {
 			const box = child.getBoundingClientRect()
 			const offset = y - box.top - box.height / 2
 			if (offset < 0 && offset > closest.offset) {
-				return { offset, element: child }
+				return { offset: offset, element: child }
 			} else {
 				return closest
 			}
@@ -59,19 +52,21 @@ function getDragEl(board, y) {
 	).element
 }
 
-// document.addEventListener('dragenter', (e) => {
+// plrBoard.addEventListener('dragenter', (e) => {
 // 	if (e.target.className === 'board-cell') {
 // 		e.target.style.opacity = 0.75
 // 		e.target.style.backgroundColor = '#70E4EF'
 // 	}
+
+// 	e.preventDefault()
 // })
 
-// document.addEventListener('dragleave', (e) => {
-// 	if (e.target.className === 'board-cell') {
-// 		e.target.style.opacity = 1
-// 		e.target.style.backgroundColor = '#fff'
-// 	}
-// })
+document.addEventListener('dragleave', (e) => {
+	if (e.target.className === 'board-cell') {
+		e.target.style.opacity = 1
+		e.target.style.backgroundColor = '#fff'
+	}
+})
 
 // document.addEventListener('drop', (e) => {
 // 	if (e.target.className === 'board-cell') {
